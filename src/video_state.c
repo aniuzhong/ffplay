aeeed737b723de8b76a72e3cf2eaf3e5da07cb67
aniuzhong <zxy_9125@163.com>
2024-05-27 15:09:40 +0800

    Update video_state.c (add queue_picture)

--- src/video_state.c
+++ src/video_state.c
@@ -14,6 +14,7 @@ static int            stream_has_enough_packets(AVStream *st, int stream_id, Pac
 static void           stream_seek(VideoState* is, int64_t pos, int64_t rel, int by_bytes);
 static int            video_thread(void *arg);
 static int            create_hwaccel(AVBufferRef** device_ctx);
+static int            queue_picture(VideoState* is, AVFrame* src_frame, double pts, double duration, int64_t pos, int serial);
 
 VideoState *stream_open(const char *filename, const AVInputFormat *iformat)
 {
@@ -774,4 +775,35 @@ static int create_hwaccel(AVBufferRef **device_ctx)
     av_log(NULL, AV_LOG_WARNING, "Derive %s from vulkan not supported.\n", hwaccel);
     ret = av_hwdevice_ctx_create(device_ctx, type, NULL, NULL, 0);
     return ret;
+}
+
+static int queue_picture(VideoState *is, AVFrame *src_frame, double pts, double duration, int64_t pos, int serial)
+{
+    Frame *vp;
+
+#if defined(DEBUG_SYNC)
+    printf("frame_type=%c pts=%0.3f\n",
+           av_get_picture_type_char(src_frame->pict_type), pts);
+#endif
+
+    if (!(vp = frame_queue_peek_writable(&is->pictq)))
+        return -1;
+
+    vp->sar = src_frame->sample_aspect_ratio;
+    vp->uploaded = 0;
+
+    vp->width = src_frame->width;
+    vp->height = src_frame->height;
+    vp->format = src_frame->format;
+
+    vp->pts = pts;
+    vp->duration = duration;
+    vp->pos = pos;
+    vp->serial = serial;
+
+    // set_default_window_size(vp->width, vp->height, vp->sar);
+
+    av_frame_move_ref(vp->frame, src_frame);
+    frame_queue_push(&is->pictq);
+    return 0;
 }
