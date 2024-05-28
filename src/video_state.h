088a83f43deb9f7d941c941d515e9da84556abea
aniuzhong <zxy_9125@163.com>
2024-05-28 15:40:59 +0800

    Update video_state.h (add fields in VideoState)

--- src/video_state.h
+++ src/video_state.h
@@ -3,55 +3,141 @@
 
 #include <libavformat/avformat.h>
 
+#include <libavfilter/avfilter.h>
+#include <libavfilter/buffersrc.h>
+#include <libavfilter/buffersink.h>
+
+#include <libavutil/avutil.h>
+#include <libavutil/bprint.h>
+#include <libavutil/time.h>
+#include <libavutil/tx.h>
+
+#include <libswscale/swscale.h>
+
+#include <libswresample/swresample.h>
+
 #include "clock.h"
 #include "packet_queue.h"
 #include "frame_queue.h"
 #include "decoder.h"
 
+/* NOTE: the size must be big enough to compensate the hardware audio buffersize size */
+/* TODO: We assume that a decoded and resampled frame fits into this buffer */
+#define SAMPLE_ARRAY_SIZE (8 * 65536)
+
+typedef struct AudioParams {
+    int freq;
+    AVChannelLayout ch_layout;
+    enum AVSampleFormat fmt;
+    int frame_size;
+    int bytes_per_sec;
+} AudioParams;
+
 typedef struct VideoState {
-    char *filename;
     SDL_Thread *read_tid;
+    const AVInputFormat *iformat;
     int abort_request;
-    SDL_cond *continue_read_thread;
+    int force_refresh;
     int paused;
+    int last_paused;
+    int queue_attachments_req;
+    int seek_req;
+    int seek_flags;
+    int64_t seek_pos;
+    int64_t seek_rel;
+    int read_pause_return;
     AVFormatContext *ic;
-    int eof;
-    int audio_stream;
-    int video_stream;
-    int subtitle_stream;
-    AVStream *audio_st;
-    AVStream *video_st;
-    AVStream *subtitle_st;
+    int realtime;
+
     Clock audclk;
     Clock vidclk;
     Clock extclk;
-    FrameQueue sampq;
+
     FrameQueue pictq;
     FrameQueue subpq;
-    PacketQueue audioq;
-    PacketQueue videoq;
-    PacketQueue subtitleq;
+    FrameQueue sampq;
+
     Decoder auddec;
     Decoder viddec;
     Decoder subdec;
-    int width;
-    int height;
+
+    int audio_stream;
+
+    int av_sync_type;
+
+    double audio_clock;
+    int audio_clock_serial;
+    double audio_diff_cum; /* used for AV difference average computation */
+    double audio_diff_avg_coef;
+    double audio_diff_threshold;
+    int audio_diff_avg_count;
+    AVStream *audio_st;
+    PacketQueue audioq;
+    int audio_hw_buf_size;
+    uint8_t *audio_buf;
+    uint8_t *audio_buf1;
+    unsigned int audio_buf_size; /* in bytes */
+    unsigned int audio_buf1_size;
+    int audio_buf_index; /* in bytes */
+    int audio_write_buf_size;
+    int audio_volume;
+    int muted;
+    struct AudioParams audio_src;
+    struct AudioParams audio_filter_src;
+    struct AudioParams audio_tgt;
+    struct SwrContext *swr_ctx;
+    int frame_drops_early;
+    int frame_drops_late;
+
     enum ShowMode {
         SHOW_MODE_NONE = -1, SHOW_MODE_VIDEO = 0, SHOW_MODE_WAVES, SHOW_MODE_RDFT, SHOW_MODE_NB
     } show_mode;
+    int16_t sample_array[SAMPLE_ARRAY_SIZE];
+    int sample_array_index;
+    int last_i_start;
+    AVTXContext *rdft;
+    av_tx_fn rdft_fn;
+    int rdft_bits;
+    float *real_data;
+    AVComplexFloat *rdft_data;
+    int xpos;
+    double last_vis_time;
+    SDL_Texture *vis_texture;
+    SDL_Texture *sub_texture;
+    SDL_Texture *vid_texture;
 
-    // Additional
-    SDL_Renderer *renderer;
+    int subtitle_stream;
+    AVStream *subtitle_st;
+    PacketQueue subtitleq;
+
+    double frame_timer;
+    double frame_last_returned_time;
+    double frame_last_filter_delay;
+    int video_stream;
+    AVStream *video_st;
+    PacketQueue videoq;
+    double max_frame_duration;      // maximum duration of a frame - above this, we consider the jump a timestamp discontinuity
+    struct SwsContext *sub_convert_ctx;
+    int eof;
+
+    char *filename;
+    int width, height, xleft, ytop;
+    int step;
+
+    int vfilter_idx;
+    AVFilterContext *in_video_filter;   // the first filter in the video chain
+    AVFilterContext *out_video_filter;  // the last filter in the video chain
+    AVFilterContext *in_audio_filter;   // the first filter in the audio chain
+    AVFilterContext *out_audio_filter;  // the last filter in the audio chain
+    AVFilterGraph *agraph;              // audio filter graph
+
+    int last_video_stream, last_audio_stream, last_subtitle_stream;
+
+    SDL_cond *continue_read_thread;
 } VideoState;
 
-VideoState    *stream_open(const char *filename);
+VideoState    *stream_open(const char *filename, const AVInputFormat *iformat);
 void           stream_close(VideoState *is);
-int            stream_component_open(VideoState *is, int stream_index);
-void           stream_component_close(VideoState *is, int stream_index);
-int            read_thread(void *arg);
-int            audio_thread(void *arg);
-int            video_thread(void *arg);
-int            subtitle_thread(void *arg);
-int            get_video_frame(VideoState *is, AVFrame *frame);
+void           toggle_pause(VideoState *is);
 
 #endif // FFPLAY_VIDEO_STATE
